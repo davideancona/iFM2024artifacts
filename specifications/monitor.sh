ce96c864d6e02a4a95552cda5003829069c522c5
davideancona <davide.ancona@unige.it>
2024-04-27 14:06:08 +0200

    specifications deleted:    specifications/monitor.sh

--- specifications/monitor.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-
-# specify monitor alias path for modules imported by the spec (like deep_subdict)
-swipl -O -p monitor=/home/davide/git/RMLatDIBRIS/monitor /home/davide/git/RMLatDIBRIS/monitor/monitor.pl -- "$@"
